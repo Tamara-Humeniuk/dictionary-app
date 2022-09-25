781320cbcaad8f6bb9bb8d577608675235c4bfbb
Tamara-Humeniuk <gumeniuk88@gmail.com>
2022-09-25 16:55:10 +0300

    Adding search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container>">
+        <header className="App-header">
+          <a href="#" className="btn btn-primary">
+            Hello
+          </a>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Tamara Humeniuk </footer>
+      </div>
     </div>
   );
 }
