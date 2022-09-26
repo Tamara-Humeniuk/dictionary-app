6742ab81b7f6126af4513ba97894551421b3771b
Tamara-Humeniuk <gumeniuk88@gmail.com>
2022-09-26 17:26:57 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container>">
-        <header className="App-header">
-          <a href="#" className="btn btn-primary">
-            Hello
-          </a>
-        </header>
+        <header className="App-header">Hello</header>
         <main>
           <Dictionary />
         </main>
