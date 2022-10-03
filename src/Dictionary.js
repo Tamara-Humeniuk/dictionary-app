86270312fc9e05d7d66b933805209f0345d0ecbb
Tamara-Humeniuk <gumeniuk88@gmail.com>
2022-10-03 16:50:34 +0300

    Adding Pexel API for photo integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results.js";
+import Photos from "./Photos.js";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
+
   function handleResponce(responce) {
     setResults(responce.data[0]);
+  }
+
+  function handlePexelResponce(responce) {
+    setPhotos(responce.data.photos);
   }
 
   function search(event) {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponce);
+    let pexelsApiKey =
+      "563492ad6f91700001000001d923d79a79394cd6b0164eb452c9c717";
+    let pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelApiUrl, { headers: headers }).then(handlePexelResponce);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -27,6 +39,7 @@ export default function Dictionary() {
         </form>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
